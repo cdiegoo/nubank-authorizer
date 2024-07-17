247b30d6b5e291a6293895ee905e4964cb35b5d9
Carlos Diego Bezerra <cdiegonascimento@gmail.com>
2024-07-17 16:32:09 -0300

    codigo incompleto

--- src/authorizer/main.py
+++ src/authorizer/main.py
@@ -1,13 +1,30 @@
 from datetime import datetime
 from models import Account, Transaction
 
-def main():
+def authorizer():
     account = Account(active=True, available_limit=1000)
-    print(account)
+    if account.active == True:
+        print("A conta está ativa!")
+        transaction = Transaction(merchant="Merchant1", amount=1000, time=datetime.now())
+        if account.history
+        if transaction.amount <= account.available_limit*0.9:
+            print("Essa transacao não excede 90 porcento do limite.")
+            if transaction.amount <= account.available_limit:
+                print("Essa transacao não excede o valor total do limite.")
+            
+            account.add_transaction(transaction)
+        else:
+            print("Deu errado") 
+    else:
+        print(account)
+
+# def main():
+#     account = Account(active=True, available_limit=1000)
+#     print(account)
     
-    transaction = Transaction(merchant="Merchant A", amount=100, time=datetime.now())
-    account.add_transaction(transaction)
-    print(account)
+#     transaction = Transaction(merchant="Merchant A", amount=100, time=datetime.now())
+#     account.add_transaction(transaction)
+#     print(account)
 
 if __name__ == "__main__":
-    main()
+    authorizer()
